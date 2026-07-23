9b5ff3b9fe5930580b85c8629550b07d79525cbe
Igsaeg <kharlfranco26@gmail.com>
2026-07-24 01:05:15 +0800

    feat: add smooth scrolling functionality with Lenis

---
+++ src/scripts/lenis.js
@@ -0,0 +1,15 @@
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+    duration: 1,
+    smoothWheel: true,
+    wheelMultiplier: 1,
+    touchMultiplier: 1.2,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
